8b19cc3ce343955d6942e6285fb4aa7f1c81a728
Steve Baker <tektsu@gmail.com>
2014-01-15 16:58:11 -0700

    Just pass relative path to methods instead of full source and target paths

--- lib/xo/directory/link.rb
+++ lib/xo/directory/link.rb
@@ -2,14 +2,13 @@ module Xo
   module Directory
     class Link
       attr_reader :source, :target
-      
       def initialize(source, target)
         @source = source
         @target = target
         @verbose = false
         @raise = false
       end
-      
+
       def verbose(state=true)
         if state
           @verbose = true
@@ -17,11 +16,11 @@ module Xo
           @verbose = false
         end
       end
-      
+
       def verbose?
         @verbose
       end
-      
+
       def raise_on_error(state=true)
         if state
           @raise = true
@@ -29,11 +28,11 @@ module Xo
           @raise = false
         end
       end
-      
+
       def raise_on_error?
         @raise
       end
-      
+
       def get_full_paths(path)
         source = @source.dup
         target = @target.dup
@@ -43,10 +42,11 @@ module Xo
         end
         return [source, target]
       end
-      
+
       def process(path=nil)
         source_path, target_path = get_full_paths(path)
         create_directory_if_needed(target_path)
+        this_level = path ? "#{path}/" : ''
 
         # Create a symlink for each file and a list of any directories
         directories = []
@@ -56,12 +56,11 @@ module Xo
           if File.directory?(source_full)
             directories.push(entry)
           else
-            create_symlink_if_needed(source_full, "#{target_path}/#{entry}")
+            create_symlink_if_needed("#{this_level}#{entry}")
           end
         end
 
         # Process any additional directories
-        this_level = path ? "#{path}/" : ''
         directories.each do |directory|
           process("#{this_level}#{directory}")
         end
@@ -71,28 +70,30 @@ module Xo
         Dir.mkdir directory unless Dir.exists? directory
       end
 
-      def create_symlink(source, target)
-        puts "Creating #{target} -> #{source}" if @verbose
-        File.unlink(target) if File.exists?(target)
-        File.symlink(source, target)
+      def create_symlink(path)
+        source_path, target_path = get_full_paths(path)
+        puts "Creating #{target_path} -> #{source_path}" if @verbose
+        File.unlink(target_path) if File.exists?(target_path)
+        File.symlink(source_path, target_path)
       end
 
-      def create_symlink_if_needed(source, target)
-        if File.exists?(target)
-          if File.symlink?(target)
-            if File.readlink(target) == source
+      def create_symlink_if_needed(path)
+        source_path, target_path = get_full_paths(path)
+        if File.exists?(target_path)
+          if File.symlink?(target_path)
+            if File.readlink(target_path) == source_path
               return
             end
           else
-            message = "#{target} exists and is not a symlink"
+            message = "#{target_path} exists and is not a symlink"
             puts message if @verbose
             raise message if @raise
             return
           end
         end
-        create_symlink(source, target)
+        create_symlink(path)
       end
-      
+
     end
   end
 end

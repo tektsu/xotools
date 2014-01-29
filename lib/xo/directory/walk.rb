be90f5ff22838f7088992cfe6695a623d7a5ecf1
Steve Baker <tektsu@gmail.com>
2014-01-28 17:27:58 -0700

    Implement callbacks

--- lib/xo/directory/walk.rb
+++ lib/xo/directory/walk.rb
@@ -7,25 +7,67 @@ module Xo
       attr_reader :directory
 
       # @param directory [String] the directory to examine
-      def initialize(directory, verbose: false)
-        @directory = directory
-        @verbose = verbose
+      def initialize(directory, ignore_hidden: false, dir_cb: nil, file_cb: nil, dfs: false)
+        @top = directory
+        @ignore_hidden = ignore_hidden
+        @dir_cb = dir_cb
+        @file_cb = file_cb
+        @dfs = dfs
       end
       
-      # Set or clear the verbose flag
+      # Process the directories recursively. Normally called initially with no parameter.
       #
-      # @param state [Boolean] the new state, true or false
+      # @param path [String] the relative path to process
       # @return [void]
-      def verbose(state=true)
-        @verbose = state ? true : false
+      def process(path='')
+        full_path = "#{@top}/#{path}"
+
+        directories = []
+        files = []
+        Dir.new(full_path).each do |entry|
+          next if entry == '.' || entry == '..'
+          next if entry =~ /^\./ && @ignore_hidden
+          full_entry = "#{full_path}/#{entry}"
+          if File.directory?(full_entry)
+            directories.push(entry)
+          else
+            files.push(entry)
+          end
+        end
+        
+        if @dfs
+          process_directories(path, directories)
+          process_files(path, files)
+        else
+          process_files(path, files)
+          process_directories(path, directories)
+        end
       end
       
-      # Get the value of the verbose flag
-      #
-      # @return [Boolean]
-      def verbose?
-        @verbose
+      private
+      
+      def process_directories(path, directories)
+        directories.each do |directory|
+          if @dir_cb
+            result = @dir_cb.call(@top, path, directory)
+            next if result == :prune
+          end
+          next_path = (path != '' ? "#{path}/" : '') << directory
+          process(next_path)
+        end
       end
+
+      def process_files(path, files)
+        files.each do |file|
+          p @top, path, file
+          @file_cb.call(@top, path, file) if @file_cb
+        end
+      end
+      
     end
   end
 end
+
+file_code = lambda {|top, path, file| puts "File: #{top}/#{path}/#{file}"}
+dir_code = lambda {|top, path, dir| puts "Dir:  #{top}/#{path}/#{dir}" ; return :prune if dir == '.git'}
+Xo::Directory::Walk.new("/Users/steve/Development/xotools", dir_cb: dir_code, file_cb: file_code).process
--- spec/lib/directory_walk_spec.rb
+++ spec/lib/directory_walk_spec.rb
@@ -13,27 +13,5 @@ describe Xo::Directory::Walk do
       walker = Xo::Directory::Walk.new('foo')
       expect(walker.directory).to eq('foo')
     end
-
-    it "is not verbose" do
-      walker = Xo::Directory::Walk.new('foo')
-      expect(walker.verbose?).to be(false)
-    end
-
-    it "is verbose when verbose has been set" do
-      walker = Xo::Directory::Walk.new('foo')
-      walker.verbose(true)
-      expect(walker.verbose?).to be(true)
-    end
-
-    it "is verbose when verbose has been set implicitly" do
-      walker = Xo::Directory::Walk.new('foo')
-      walker.verbose
-      expect(walker.verbose?).to be(true)
-    end
-
-    it "is verbose when verbose has been set through the constructor" do
-      walker = Xo::Directory::Walk.new('foo', verbose: true)
-      expect(walker.verbose?).to be(true)
-    end
   end
 end

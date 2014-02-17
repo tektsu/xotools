1e454fdb40bec128559c14de21bc735f6ce35274
Steve Baker <tektsu@gmail.com>
2014-02-17 07:27:32 -0700

    Add documentation

--- lib/xo/directory/dvd.rb
+++ lib/xo/directory/dvd.rb
@@ -6,8 +6,8 @@ module Xo
     
     class Dvd
       
-      attr_reader :path, :max_size, :current_size
-      
+      # @param path [String] the directory to create for this dvd (must not exist)
+      # @param size [Integer] the maximum number of bytes allowed in the directory
       def initialize(path, size=(4.2 * 1024 * 1024 * 1024))
         @path = File.expand_path(path)
         raise "Directory path #{@path} already exists" if File.exists?(@path)
@@ -16,10 +16,32 @@ module Xo
         @current_size = 0
       end
       
+      # Get the path to the dvd directory
+      #
+      # @return [String]
+      attr_reader :path
+
+      # Get the maximum number of bytes allowed in this directory
+      #
+      # @return [Integer]
+      attr_reader :max_size
+
+      # Get the current number of bytes in this directory
+      #
+      # @return [Integer]
+      attr_reader :current_size
+      
+      # Get the number of free bytes remaining in the directory
+      #
+      # @return [Integer]
       def remaining_space
         @max_size - @current_size
       end
-      
+
+      # Add a file or directory to the dvd directory
+      #
+      # @param path [String] the location of file or directory to add
+      # @param size [Integer] the size of the file or directory if already known      
       def add(path, size=nil)
         raise "Add path #{path} does not exist" unless File.exists?(path)
         if Dir.exists?(path)

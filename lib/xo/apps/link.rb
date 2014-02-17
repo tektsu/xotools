a221effe9a0ec59bc0b4751d33541b70bb199830
Steve Baker <tektsu@gmail.com>
2014-02-17 07:27:10 -0700

    Move application implementation classes to their own directory

--- lib/xo/apps/link.rb
+++ lib/xo/apps/link.rb
@@ -2,49 +2,51 @@ require 'optparse'
 require 'xo/directory/link'
 
 module Xo
-  class Link
-    def initialize
-      @options = {
-        :package_dir => "/opt/xecko",
-        :verbose => true,
-      }
-
-      parse_options
-    end
-
-    def run
-      get_packages_to_link.each do |package|
-        linker = Xo::Directory::Link.new("#{@options[:package_dir]}/#{package}", "#{@options[:package_dir]}")
-        linker.verbose(@options[:verbose])
-        linker.link
+  module Apps
+    
+    class Link
+      def initialize
+        @options = {
+          :package_dir => "/opt/xecko",
+          :verbose => true,
+        }
+  
+        parse_options
       end
-    end
-
-    private
-
-    def parse_options
-      OptionParser.new do |opts|
-        opts.banner = "Usage: xolink [options]"
-        opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
-          @options[:verbose] = v
+  
+      def run
+        get_packages_to_link.each do |package|
+          linker = Xo::Directory::Link.new("#{@options[:package_dir]}/#{package}", "#{@options[:package_dir]}")
+          linker.verbose(@options[:verbose])
+          linker.link
         end
-        opts.on("-p", "--package-dir DIR", "Specify package directory") do |dir|
-          @options[:package_dir] = dir
-        end
-      end.parse!
-    end
-
-    def get_packages_to_link
-      packages_to_link = []
-      Dir.new(@options[:package_dir]).each do |entry|
-        next if entry =~ /^\./
-        next unless entry =~ /-/
-        next unless File.directory?("#{@options[:package_dir]}/#{entry}")
-        packages_to_link.push(entry)
       end
-      packages_to_link
+  
+      private
+  
+      def parse_options
+        OptionParser.new do |opts|
+          opts.banner = "Usage: xolink [options]"
+          opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+            @options[:verbose] = v
+          end
+          opts.on("-p", "--package-dir DIR", "Specify package directory") do |dir|
+            @options[:package_dir] = dir
+          end
+        end.parse!
+      end
+  
+      def get_packages_to_link
+        packages_to_link = []
+        Dir.new(@options[:package_dir]).each do |entry|
+          next if entry =~ /^\./
+          next unless entry =~ /-/
+          next unless File.directory?("#{@options[:package_dir]}/#{entry}")
+          packages_to_link.push(entry)
+        end
+        packages_to_link
+      end
+  
     end
-
   end
-
 end
